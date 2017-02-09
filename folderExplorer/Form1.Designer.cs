959a59fe52a169cd84b048eaec707c3ba8e68496
andyNg <tynshjt@gmail.com>
2017-02-09 02:29:30 +0200

    Add project files.

---
+++ folderExplorer/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace folderExplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(37, 37);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(391, 324);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseDoubleClick_1);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "images.jpg");
+            this.imageList1.Images.SetKeyName(1, "point-on-map-512.png");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(390, 368);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Duong Nguyen";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 390);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Explorer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ folderExplorer/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace folderExplorer
+{
+    public partial class Form1 : Form
+    {
+        string path;
+        public Form1()
+        {
+            InitializeComponent();
+            path = @"D:\study\online courses";
+            TreeNode root = new TreeNode();
+            root.Text = path;
+            treeView1.ImageList = imageList1;
+            treeView1.SelectedImageIndex = 1;
+            treeView1.Nodes.Add(root);
+            explorer(root);
+        }
+
+        private void explorer(TreeNode node)
+        {
+            #region getfile
+            // try get file list
+            try
+            {
+                string[] fileList = Directory.GetFiles(node.Text);
+                //MessageBox.Show(fileList.Length.ToString());
+                foreach (string file in fileList)
+                {
+                    //get icon of the file
+                    Icon icon1 = Icon.ExtractAssociatedIcon(file);
+                    //add the icon to the imageList1 control
+                    imageList1.Images.Add(icon1);
+                    //set the treeview imageList is out imageList1
+                    treeView1.ImageList = imageList1;
+                    //create a new node for file and add attribute to it: text is file path and image is the icon just added
+                    TreeNode fileNode = new TreeNode() { Text = file, ImageIndex = imageList1.Images.Count - 1};
+                    node.Nodes.Add(fileNode);
+                    //imageList1.Images.RemoveAt(imageList1.Images.Count);
+                   
+                }
+            }
+            catch
+            {
+                new FileNotFoundException();
+            }
+
+            #endregion getfile
+            // try get folderlist
+            try
+            {
+                
+
+                System.IO.DirectoryInfo folder = new DirectoryInfo(node.Text);
+                DirectoryInfo[] folderList = folder.GetDirectories();
+                
+                foreach (var item in folderList)
+                {
+
+                    
+
+                    TreeNode Node = new TreeNode() { Text = item.FullName, ImageIndex = 0 };
+                    node.Nodes.Add(Node);
+                    explorer(Node);
+                }
+            }
+            catch
+            {
+                return;
+            }
+
+
+
+        } //end of explorer method
+
+       
+
+        private void treeView1_NodeMouseDoubleClick_1(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            try
+            {
+                // Look for a file extension.
+                if (e.Node.Text.Contains("."))
+                    System.Diagnostics.Process.Start(e.Node.Text);
+            }
+            // If the file is not found, handle the exception and inform the user.
+            catch (System.ComponentModel.Win32Exception)
+            {
+                MessageBox.Show("File not found.");
+            }
+        }
+    }
+}
